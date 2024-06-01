8aec43807dab570b1319c6e107148c682007f6ce
apmaso <amaso@pdx.edu>
2024-05-31 22:15:39 -0700

    removed if() from driver

--- M4/UVM/classes/driver.sv
+++ M4/UVM/classes/driver.sv
@@ -41,18 +41,11 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
     forever begin
       seq_item_port.get_next_item(tx_wr); 
       // Drive data to FIFO
-      if (tx_wr.wr_en) begin
-        @(posedge bfm.clk_wr);
-          bfm.data_in <= tx_wr.data_in;
-          bfm.wr_en <= tx_wr.wr_en;
-      end
-      else if (tx_wr.rd_en) begin
-        @(posedge bfm.clk_rd);
-          bfm.rd_en <= tx_wr.rd_en;
-      end
-      else begin
-        `uvm_error("DRIVER", "Neither read nor write enabled");
-      end
+      @(posedge bfm.clk_wr);
+        bfm.data_in <= tx_wr.data_in;
+        bfm.wr_en <= tx_wr.wr_en;
+      @(posedge bfm.clk_rd);
+        bfm.rd_en <= tx_wr.rd_en;
          
       `uvm_info(get_type_name(), $sformatf("Driver tx_wr \t\t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  ", tx_wr.wr_en, tx_wr.rd_en, tx_wr.data_in), UVM_MEDIUM);
       seq_item_port.item_done(); 

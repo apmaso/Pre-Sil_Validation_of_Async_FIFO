2b0038bda35c2ab4e3e76869c0b8ecb5c8d405c5
apmaso <amaso@pdx.edu>
2024-06-08 23:41:37 -0700

    SCB: Moved f/e/h counters to respective write_ports

--- M5/UVM/classes/scoreboard.sv
+++ M5/UVM/classes/scoreboard.sv
@@ -105,20 +105,44 @@ class fifo_scoreboard extends uvm_scoreboard;
     endtask: run_phase
 
     function void write_port_a(fifo_transaction mon_tx_wr);
-        // Bump counter and display count`
+        // Bump counters and display counts
         write_count++;
         `uvm_info("SCOREBOARD", $sformatf("Write count: %0d", write_count), UVM_MEDIUM);
-        
+        if (mon_tx_wr.full) begin
+            full_count_wr++;
+            `uvm_info("SCOREBOARD", $sformatf("Full write count: %0d", full_count_wr), UVM_MEDIUM);
+        end
+        if (mon_tx_wr.empty) begin
+            empty_count_wr++;
+            `uvm_info("SCOREBOARD", $sformatf("Empty write count: %0d", empty_count_wr), UVM_MEDIUM);
+        end
+        if (mon_tx_wr.half) begin
+            half_count_wr++;
+            `uvm_info("SCOREBOARD", $sformatf("Half write count: %0d", half_count_wr), UVM_MEDIUM);
+        end
+
         // Push the write transaction onto the stack
         tx_stack_wr.push_back(mon_tx_wr);
         `uvm_info(get_type_name(), $sformatf("Scoreboard tx \t|  wr_en: %b  |  data_in: %h  |", mon_tx_wr.wr_en, mon_tx_wr.data_in), UVM_HIGH);
     endfunction : write_port_a
 
     function void write_port_b(fifo_transaction mon_tx_rd);
-        // Bump counter and display count
+        // Bump counters and display counts
         read_count++;
         `uvm_info("SCOREBOARD", $sformatf("Read count: %0d", read_count), UVM_MEDIUM);
- 
+        if (mon_tx_rd.full) begin
+            full_count_rd++;
+            `uvm_info("SCOREBOARD", $sformatf("Full read count: %0d", full_count_rd), UVM_MEDIUM);
+        end
+        if (mon_tx_rd.empty) begin
+            empty_count_rd++;
+            `uvm_info("SCOREBOARD", $sformatf("Empty read count: %0d", empty_count_rd), UVM_MEDIUM);
+        end
+        if (mon_tx_rd.half) begin
+            half_count_rd++;
+            `uvm_info("SCOREBOARD", $sformatf("Half read count: %0d", half_count_rd), UVM_MEDIUM);
+        end
+
         // Push the read transaction onto the stack
         tx_stack_rd.push_back(mon_tx_rd);
         `uvm_info(get_type_name(), $sformatf("Scoreboard tx \t|  rd_en: %b  |  data_out: %h  |", mon_tx_rd.rd_en, mon_tx_rd.data_out), UVM_HIGH);
